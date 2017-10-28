2c4d8936df66ed789599e75ca07fb9484646f68d
Hodobox <Hodobox@SGLaptop.localdomain>
2017-10-28 15:17:48 +0100

    World module

---
+++ World.py
@@ -0,0 +1,50 @@
+import json
+import requests
+
+class World:
+	def __init__(self,url="http://0.0.0.0:6001"):
+		self.url = url
+
+	def get_world(self,distance = 1000):
+
+		r = requests.get(self.url + "/api/world/objects", params = {'distance': distance})
+		if r.status_code != 200:
+			print("world/objects request error: " + r.text)
+			return {}
+
+		world_objects = json.loads(r.text)
+
+		return world_objects
+
+	def get_monsters(self,distance = 1000):
+
+		world_objects = self.get_world(distance)
+		monsters = []
+		for obj in world_objects:
+			name = obj['type']
+			if name.isupper():
+				monsters.append(obj)
+
+		return monsters
+
+	def get_doors(self):
+		r = requests.get(self.url + "/api/world/doors")
+
+		if r.status_code != 200:
+			print("world/doors request error: " + r.text)
+			return {}
+
+		doors = json.loads(r.text)
+		return doors
+
+	def get_pickups(self,distance = 1000):
+
+		world_objects = get_world(distance)
+		pickups = []
+
+		for obj in world_objects:
+			name = obj['type']
+			if 'armor' in name or 'health' in name:
+				pickups.append(obj)
+
+		return pickups
